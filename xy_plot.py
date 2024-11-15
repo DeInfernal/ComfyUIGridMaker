d0b810bdc449dc36702cb606055492cf90469495
Project Sixth <six@6sphere.ru>
2024-11-15 13:24:34 +0300

    Fix: Flag was in the wrong direction.

--- xy_plot.py
+++ xy_plot.py
@@ -9,7 +9,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--comfyui_ip", type=str, help="IP of ComfyUI interface", default="127.0.0.1")
     parser.add_argument("--comfyui_port", type=str, help="Port of ComfyUI interface", default="8188")
-    parser.add_argument("--skip_mass_generation", action="store_false", help="Set the flag to skip the step with generation of XY plot (only make images)")
+    parser.add_argument("--skip_mass_generation", action="store_true", help="Set the flag to skip the step with generation of XY plot (only make images)")
     parser.add_argument("--ignore_non_replacements", action="store_true", help="Set the flag to True if you are fine that on Axis variable replacement step there can be no replacements to workflow")
     parser.add_argument("--resize_ratio", type=float, help="Set to number lower than 1 to scale down the XY plot (useful if you have thousands of HD images)")
     parser.add_argument("--autoreduce", type=int, help="Set the flag to automatically lower the XY grid size to a (your specified number) pixels if it was above such number.")
