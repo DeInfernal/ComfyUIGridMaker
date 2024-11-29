f720906b1f79c6b5e2291d2e9a23147cfb9f1710
Project Sixth <six@6sphere.ru>
2024-11-29 15:52:38 +0300

    Added support for recursive plot generation

--- lib/plotfile.py
+++ lib/plotfile.py
@@ -39,6 +39,7 @@ class PlotFile:
     workflow_stencil = None
     axises = None
     variables = None
+    system_variables = None
     output_folder_name = None
     output_file_suffix = None
     resize_ratio = None
@@ -63,44 +64,45 @@ class PlotFile:
         if "Variables" not in self.content:
             print("Variables dictionary not found - making it empty.")
         self.variables = self.content.get("Variables", dict())
+        self.system_variables = dict()
 
         if "Image_Width" not in self.content:
             raise Exception("A correct Image_Width must be present in PlotFile.")
-        self.variables.setdefault("IMAGE_WIDTH", self.content.get("Image_Width"))
-        self.variables.setdefault("IMAGE_ONEANDATHIRDOFHALVED_WIDTH", int(self.content.get("Image_Width")*0.66))
-        self.variables.setdefault("IMAGE_ONEANDAHALFOFHALVED_WIDTH", int(self.content.get("Image_Width")*0.75))
-        self.variables.setdefault("IMAGE_ONEANDTWOTHIRDSOFHALVED_WIDTH", int(self.content.get("Image_Width")*0.83))
-        self.variables.setdefault("IMAGE_ONEANDATHIRD_WIDTH", int(self.content.get("Image_Width")*1.33))
-        self.variables.setdefault("IMAGE_ONEANDTWOTHIRDS_WIDTH", int(self.content.get("Image_Width")*1.66))
-        self.variables.setdefault("IMAGE_ONEANDAHALF_WIDTH", int(self.content.get("Image_Width")*1.5))
-        self.variables.setdefault("IMAGE_DOUBLE_WIDTH", self.content.get("Image_Width")*2)
-        self.variables.setdefault("IMAGE_QUAD_WIDTH", self.content.get("Image_Width")*4)
-        self.variables.setdefault("IMAGE_HALF_WIDTH", int(self.content.get("Image_Width")/2))
-        self.variables.setdefault("IMAGE_QUARTER_WIDTH", int(self.content.get("Image_Width")/4))
+        self.system_variables.setdefault("IMAGE_WIDTH", self.content.get("Image_Width"))
+        self.system_variables.setdefault("IMAGE_ONEANDATHIRDOFHALVED_WIDTH", int(self.content.get("Image_Width")*0.66))
+        self.system_variables.setdefault("IMAGE_ONEANDAHALFOFHALVED_WIDTH", int(self.content.get("Image_Width")*0.75))
+        self.system_variables.setdefault("IMAGE_ONEANDTWOTHIRDSOFHALVED_WIDTH", int(self.content.get("Image_Width")*0.83))
+        self.system_variables.setdefault("IMAGE_ONEANDATHIRD_WIDTH", int(self.content.get("Image_Width")*1.33))
+        self.system_variables.setdefault("IMAGE_ONEANDTWOTHIRDS_WIDTH", int(self.content.get("Image_Width")*1.66))
+        self.system_variables.setdefault("IMAGE_ONEANDAHALF_WIDTH", int(self.content.get("Image_Width")*1.5))
+        self.system_variables.setdefault("IMAGE_DOUBLE_WIDTH", self.content.get("Image_Width")*2)
+        self.system_variables.setdefault("IMAGE_QUAD_WIDTH", self.content.get("Image_Width")*4)
+        self.system_variables.setdefault("IMAGE_HALF_WIDTH", int(self.content.get("Image_Width")/2))
+        self.system_variables.setdefault("IMAGE_QUARTER_WIDTH", int(self.content.get("Image_Width")/4))
 
         if "Image_Height" not in self.content:
             raise Exception("A correct Image_Height must be present in PlotFile.")
-        self.variables.setdefault("IMAGE_HEIGHT", self.content.get("Image_Height"))
-        self.variables.setdefault("IMAGE_ONEANDATHIRDOFHALVED_HEIGHT", int(self.content.get("Image_Height")*0.66))
-        self.variables.setdefault("IMAGE_ONEANDAHALFOFHALVED_HEIGHT", int(self.content.get("Image_Height")*0.75))
-        self.variables.setdefault("IMAGE_ONEANDTWOTHIRDSOFHALVED_HEIGHT", int(self.content.get("Image_Height")*0.83))
-        self.variables.setdefault("IMAGE_ONEANDATHIRD_HEIGHT", int(self.content.get("Image_Height")*1.33))
-        self.variables.setdefault("IMAGE_ONEANDTWOTHIRDS_HEIGHT", int(self.content.get("Image_Height")*1.66))
-        self.variables.setdefault("IMAGE_ONEANDAHALF_HEIGHT", int(self.content.get("Image_Height")*1.5))
-        self.variables.setdefault("IMAGE_DOUBLE_HEIGHT", self.content.get("Image_Height")*2)
-        self.variables.setdefault("IMAGE_QUAD_HEIGHT", self.content.get("Image_Height")*4)
-        self.variables.setdefault("IMAGE_HALF_HEIGHT", int(self.content.get("Image_Height")/2))
-        self.variables.setdefault("IMAGE_QUARTER_HEIGHT", int(self.content.get("Image_Height")/4))
+        self.system_variables.setdefault("IMAGE_HEIGHT", self.content.get("Image_Height"))
+        self.system_variables.setdefault("IMAGE_ONEANDATHIRDOFHALVED_HEIGHT", int(self.content.get("Image_Height")*0.66))
+        self.system_variables.setdefault("IMAGE_ONEANDAHALFOFHALVED_HEIGHT", int(self.content.get("Image_Height")*0.75))
+        self.system_variables.setdefault("IMAGE_ONEANDTWOTHIRDSOFHALVED_HEIGHT", int(self.content.get("Image_Height")*0.83))
+        self.system_variables.setdefault("IMAGE_ONEANDATHIRD_HEIGHT", int(self.content.get("Image_Height")*1.33))
+        self.system_variables.setdefault("IMAGE_ONEANDTWOTHIRDS_HEIGHT", int(self.content.get("Image_Height")*1.66))
+        self.system_variables.setdefault("IMAGE_ONEANDAHALF_HEIGHT", int(self.content.get("Image_Height")*1.5))
+        self.system_variables.setdefault("IMAGE_DOUBLE_HEIGHT", self.content.get("Image_Height")*2)
+        self.system_variables.setdefault("IMAGE_QUAD_HEIGHT", self.content.get("Image_Height")*4)
+        self.system_variables.setdefault("IMAGE_HALF_HEIGHT", int(self.content.get("Image_Height")/2))
+        self.system_variables.setdefault("IMAGE_QUARTER_HEIGHT", int(self.content.get("Image_Height")/4))
 
         if "OutputFolderName" not in self.content:
             print("No OutputFolderName present in PlotFile. Outputing results into folder named 'Generic'")
         self.output_folder_name = self.content.get("OutputFolderName", "Generic")
-        self.variables.setdefault("OUTPUT_FOLDER_NAME", self.output_folder_name)
+        self.system_variables.setdefault("OUTPUT_FOLDER_NAME", self.output_folder_name)
 
         if "OutputFileSuffix" not in self.content:
             print("No OutputFileSuffix present in PlotFile. It is set to None.")
         self.output_file_suffix = self.content.get("OutputFileSuffix", "")
-        self.variables.setdefault("OUTPUT_FILE_SUFFIX", self.output_file_suffix)
+        self.system_variables.setdefault("OUTPUT_FILE_SUFFIX", self.output_file_suffix)
 
         if "WorkflowPath" not in self.content:
             raise Exception("A correct WorkflowPath must be present in PlotFile.")
@@ -161,13 +163,74 @@ class PlotFile:
     def set_do_hash_filenames(self, new_do_hash_filenames):
         self.do_hash_filenames = new_do_hash_filenames
 
+    def _replace_system_variables(self, string_workflow):
+        replacement_amount = 0
+        resulting_string_workflow = string_workflow
+
+        for variable in self.system_variables:
+            new_string_workflow = resulting_string_workflow.replace(variable, str(self.system_variables.get(variable)))
+            if new_string_workflow != resulting_string_workflow:
+                replacement_amount += 1
+            resulting_string_workflow = new_string_workflow
+        
+        return resulting_string_workflow, replacement_amount
+
+    def _replace_static_variables(self, string_workflow):
+        replacement_amount = 0
+        resulting_string_workflow = string_workflow
+
+        for variable in self.variables:
+            new_string_workflow = resulting_string_workflow.replace(variable, str(self.variables.get(variable)))
+            if new_string_workflow != resulting_string_workflow:
+                replacement_amount += 1
+            resulting_string_workflow = new_string_workflow
+        
+        return resulting_string_workflow, replacement_amount
+
+    def _replace_dynamic_variables(self, string_workflow, values: dict):
+        replacement_amount = 0
+        resulting_string_workflow = string_workflow
+
+        for value in values:
+            new_string_workflow = resulting_string_workflow.replace(value, str(values.get(value)))
+            if new_string_workflow != resulting_string_workflow:
+                replacement_amount += 1
+            resulting_string_workflow = new_string_workflow
+        
+        return resulting_string_workflow, replacement_amount
+
+    def _replace_all_variables(self, string_workflow, values: dict):
+        t_string_workflow, firstrepl = self._replace_system_variables(string_workflow)
+        t_string_workflow, secondrepl = self._replace_static_variables(string_workflow)
+        t_string_workflow, thirdrepl = self._replace_dynamic_variables(t_string_workflow, values)
+        return t_string_workflow, firstrepl + secondrepl + thirdrepl
+
     def generate_workflow(self, values: dict):
         string_workflow = self.workflow_stencil
-        # Replace static variables.
-        for variable in self.variables:
-            string_workflow = string_workflow.replace(variable, str(self.variables.get(variable)))
+        # Initially replace system variables. It is okay if nothing gets replaced, since not every system variable is used.
+        string_workflow, _ = self._replace_system_variables(string_workflow)
 
-        # Replace dynamic variables.
+        # Initially replace static variables.
+        for variable in self.variables:
+            new_string_workflow = string_workflow.replace(variable, str(self.variables.get(variable)))
+            
+            if not self.ignore_non_replacements and new_string_workflow == string_workflow:
+                print("ERROR! HALT! ERROR! HALT! ERROR! HALT!")
+                print("HALT! ERROR! HALT! ERROR! HALT! ERROR!")
+                print("ERROR! HALT! ERROR! HALT! ERROR! HALT!")
+                print()
+                print("After initial replacing {} static/Axis variable, no changes were made to the workflow!".format(variable))
+                print("This indicates that something is wrong with the Plotfile.")
+                print("Please recheck the plotfile manually.")
+                print("If this is desired behavior, run the script")
+                print("with flag --ignore_non_replacements .")
+                print()
+                input("Press enter to exit program.")
+                exit(0)
+                
+            string_workflow = new_string_workflow
+
+        # Initially replace dynamic variables.
         for value in values:
             new_string_workflow = string_workflow.replace(value, str(values.get(value)))
 
@@ -176,7 +239,7 @@ class PlotFile:
                 print("HALT! ERROR! HALT! ERROR! HALT! ERROR!")
                 print("ERROR! HALT! ERROR! HALT! ERROR! HALT!")
                 print()
-                print("After replacing {} dynamic/Axis variable, no changes were made to the workflow!".format(value))
+                print("After initial replacing {} dynamic/Axis variable, no changes were made to the workflow!".format(value))
                 print("This indicates that something is wrong with the Plotfile.")
                 print("Please recheck the plotfile manually.")
                 print("If this is desired behavior, run the script")
@@ -187,7 +250,26 @@ class PlotFile:
 
             string_workflow = new_string_workflow
 
+        # Start replacing variables recursively until either no replacements are made or replacements exceeded threshold of 128
+        replacement_cycles = 0
+        string_workflow, last_replacement_count = self._replace_all_variables(string_workflow, values)
+        while replacement_cycles < 128 and last_replacement_count > 0:
+            replacement_cycles += 1
+            string_workflow, last_replacement_count = self._replace_all_variables(string_workflow, values)
+        
+        if replacement_cycles > 127:
+            print("ERROR! HALT! ERROR! HALT! ERROR! HALT!")
+            print("HALT! ERROR! HALT! ERROR! HALT! ERROR!")
+            print("ERROR! HALT! ERROR! HALT! ERROR! HALT!")
+            print()
+            print("After cycling replacmement of 127 cycles, script still found something to replace")
+            print("This indicates recursive replacement somewhere. Fix it manually.")
+            print()
+            input("Press enter to exit program.")
+            exit(0) 
+
         # Render workflow.
+        # print(string_workflow)  # Debugging purposes
         rendered_workflow = json.loads(string_workflow)
 
         # Return generated workflow.
